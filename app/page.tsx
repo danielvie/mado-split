a05fdc1e915c0cdabcf37b44cb456c8b193b9a52
Daniel Vieira <danielvie@gmail.com>
2023-05-01 08:57:11 +0200

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,117 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from "next/head"
 
-const inter = Inter({ subsets: ['latin'] })
+
+interface TAction {
+    id: number;
+    description: string;
+    paidby: User;
+    split?: User[];
+    split_manual?: number[];
+    amount: number;
+}
+
+enum User {
+    DANIEL = 0,
+    SAMUEL = 1,
+    MARCELO = 2,
+}
+
+const data: TAction[] = [
+    {id: 1, description: "Batatas", paidby: User.MARCELO, amount: 18.0, split: [User.DANIEL, User.SAMUEL, User.MARCELO], split_manual: [1, 1, 2] },
+    {id: 2, description: "Rabawn Beer", paidby: User.DANIEL, amount: 30.0, split: [User.DANIEL, User.MARCELO] },
+    {id: 3, description: "Comidas", paidby: User.MARCELO, amount: 41.0, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+    {id: 4, description: "Canecao Br Roots", paidby: User.SAMUEL, amount: 25.5, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+    {id: 5, description: "Cerveja Br Roots", paidby: User.DANIEL, amount: 16.0, split: [User.DANIEL, User.SAMUEL] },
+    {id: 6, description: "Chopp", paidby: User.SAMUEL, amount: 18.0, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+    {id: 7, description: "Breja na rua", paidby: User.SAMUEL, amount: 10.0, split: [User.DANIEL, User.SAMUEL] },
+    {id: 8, description: "Cerveja na rua", paidby: User.DANIEL, amount: 24.0, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+    {id: 9, description: "Heinekens", paidby: User.SAMUEL, amount: 17.5, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+    {id: 10, description: "Kebab", paidby: User.SAMUEL, amount: 14.0, split: [User.DANIEL, User.SAMUEL] },
+    {id: 11, description: "Caipirinhas", paidby: User.MARCELO, amount: 50.0, split: [User.DANIEL, User.SAMUEL, User.MARCELO], split_manual: [1, 1, 2] },
+    {id: 12, description: "Chopp", paidby: User.MARCELO, amount: 22.5, split: [User.DANIEL, User.SAMUEL, User.MARCELO] },
+]
+
+const CalculateCost = (user: User) => {
+    let res: number = 0
+    
+    data.forEach((a) => {
+
+        // creating igual division if `split_manual` dont exist
+        if (!a.split_manual?.length) {
+            a.split_manual = Array(a.split?.length).fill(1)
+        }
+
+        // calculating factor
+        const nt_: number = a.split_manual.reduce((m,n) => m + n)
+        let pos_: number = 0
+        a.split?.forEach((ai, i) => {if (ai == user) pos_ = i})
+
+        const factor: number = a.split_manual[pos_] / nt_
+
+        // compute split
+        if (!a.split || a.split?.includes(user)) {
+            const value = a.amount * factor
+            res += value
+        }
+        
+        // remove payed amount
+        if (a.paidby == user) {
+            res -= a.amount
+        }
+
+    })	
+
+    return res.toFixed(2)
+}
+
+const CalculateTotal = () => {
+    let total: number = 0.
+
+    data.forEach((a) => {
+        total += a.amount
+    })
+    
+    return total
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className="bg-sky-950 text-gray-300 min-h-screen">
+            <Head>
+                <title>Movie</title>
+                <meta name='' content="a simple movie database" />
+            </Head>
+            
+            <main className="container mx-auto py-10 px-4 flex flex-col items-center justify-center">
+                <h1 className="text-4x1 font-bold">asdfasdf</h1>
+
+
+
+                
+                <ul role="list" className="divide-y divide-sky-500">
+
+                    {data.map((el) => 
+                        <li key={el.id} className="flex justify-between gap-x-6 py-5">
+                            <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6 text-gray-300">{el.description}</p>
+                            </div>
+                            <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6 text-gray-300">{User[el.paidby]}</p>
+                            </div>
+                            <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                <p className="mt-1 truncate text-xs leading-5 text-gray-200">{el.amount}</p>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+
+                
+                <p>Marcelo: {CalculateCost(User.MARCELO)}</p>
+                <p>Samuel : {CalculateCost(User.SAMUEL)}</p>
+                <p>Daniel : {CalculateCost(User.DANIEL)}</p>
+                <p>Total: {CalculateTotal()}</p>
+                
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
